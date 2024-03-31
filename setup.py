3761cb996c6f2667fc1442d2b6d67b1d413a9f0e
Daniel Hsu <danny@Daniels-MacBook-Pro.local>
2024-03-31 14:05:45 -0700

    Update setup.py to remove explicit pip requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
-
 
 def get_requirements(source):
     """Get the requirements from the given ``source``
@@ -18,17 +7,14 @@ def get_requirements(source):
     ----------
     source: str
         The filename containing the requirements
-
     """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+    with open(source, 'r') as f:
+        requirements = f.readlines()
+    # Remove any comments or empty lines
+    requirements = [r.strip() for r in requirements if r.strip() and not r.startswith('#')]
+    return requirements
 
 setup(
     packages=find_packages(),
     install_requires=get_requirements('requirements/requirements.txt')
 )
-
-
